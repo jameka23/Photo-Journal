c14b0f30aed8ea129823cf1dd9879b1d7a804f62
Jameka Echols <jameka.echols@gmail.com>
2019-02-24 19:07:53 -0600

    Fixed the modal in photos. Added js to journal.js.

--- journal.js
+++ journal.js
@@ -2,10 +2,14 @@
 let contactFirstName = document.getElementById("contactFirstName");
 let contactLastName = document.getElementById("contactLastName");
 let contactEmail = document.getElementById("contactEmail");
+let form = document.getElementById("contactMe");
 
+// addEventListener
+form.addEventListener("submit", output);
 
 // Function to console.log user contact info 
 function output(contactFirstName, contactLastName, contactEmail) {
     console.log("The user's contact information is:");
     console.log("Name: " + contactFirstName + " " + contactLastName + " Email: " + contactEmail + ".");
 }
+
