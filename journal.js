5a04970bedaadde6f82b4b5fb23fae09564e3886
Jameka Echols <jameka.echols@gmail.com>
2019-02-28 10:01:24 -0600

    fixed the first modal in the photos.html

--- journal.js
+++ journal.js
@@ -3,9 +3,9 @@ let form = document.getElementById("formContact");
 
 // Function to console.log user contact info 
 function output() {
-    let contactFirstName = document.getElementById("contactFirstName").value;
-    let contactLastName = document.getElementById("contactLastName").value;
-    let contactEmail = document.getElementById("contactEmail").value;
+    const contactFirstName = document.getElementById("contactFirstName").value;
+    const contactLastName = document.getElementById("contactLastName").value;
+    const contactEmail = document.getElementById("contactEmail").value;
     console.log("The user's contact information is:");
     console.log("Name: " + contactFirstName + " " + contactLastName + " Email: " + contactEmail + ".");
 }
