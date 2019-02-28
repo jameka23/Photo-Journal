fb77b634f93c701dc7087ac8950baeffd457b59c
Jameka Echols <jameka.echols@gmail.com>
2019-02-28 10:10:05 -0600

    Changed the variables in js file from let to const

--- journal.js
+++ journal.js
@@ -1,11 +1,22 @@
 // Variable Declarations
 let form = document.getElementById("formContact");
+// let contactFirstName = document.getElementById("contactFirstName");
+// let contactLastName = document.getElementById("contactLastName");
+// let contactEmail = document.getElementById("contactEmail");
 
 // Function to console.log user contact info 
+// function output() {
+//     // let contactFirstName = document.getElementById("contactFirstName").value;
+//     // let contactLastName = document.getElementById("contactLastName").value;
+//     // let contactEmail = document.getElementById("contactEmail").value;
+//     console.log("The user's contact information is:");
+//     console.log("Name: " + contactFirstName.value + " " + contactLastName.value + " Email: " + contactEmail.value + ".");
+// }
+
 function output() {
-    const contactFirstName = document.getElementById("contactFirstName").value;
-    const contactLastName = document.getElementById("contactLastName").value;
-    const contactEmail = document.getElementById("contactEmail").value;
+    let contactFirstName = document.getElementById("contactFirstName").value;
+    let contactLastName = document.getElementById("contactLastName").value;
+    let contactEmail = document.getElementById("contactEmail").value;
     console.log("The user's contact information is:");
     console.log("Name: " + contactFirstName + " " + contactLastName + " Email: " + contactEmail + ".");
 }
